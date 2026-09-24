57730549b32887995c94215d1fd4109e544c1d9a
Libor Šaja <sajalibor@gmail.com>
2026-09-24 20:08:04 +0200

    Layout adjustments
    Layout adjustments and add author footer

--- src/constants/ApiEndpoints.js
+++ src/constants/ApiEndpoints.js
@@ -2,8 +2,8 @@
  * Local resource endpoints and external API endpoints.
  */
 export const API_ENDPOINTS = Object.freeze({
-    CITIES_DATASET: "/data/city.list.json",
-    TRANSLATIONS_DATASET: "/data/translations.csv",
+    CITIES_DATASET: "./data/city.list.json",
+    TRANSLATIONS_DATASET: "./data/translations.csv",
     OPENWEATHER_FORECAST_BASE_URL:
         "https://api.openweathermap.org/data/2.5/forecast",
     OPENWEATHER_ICON_BASE_URL: "https://openweathermap.org/img/wn/",
